a9c3e5424fb67a92f7d958633125b129dea3ca92
Dobrycy <bartoszklodka@wp.pl>
2020-01-02 18:43:09 +0100

    migracje

---
+++ database/migrations/2019_12_05_194553_create_questions_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('textOfQuestion');
+            $table->string('answerA');
+            $table->string('answerB');
+            $table->string('answerC');
+            $table->string('answerD');
+            $table->char('correctAnswer');
+            $table->integer('category')->unsigned();
+            $table->foreign('category')->references('id')->on('categories');
+            $table->integer('language')->unsigned();
+            $table->foreign('language')->references('id')->on('languages');
+            $table->integer('added_by_user')->unsigned();
+            $table->foreign('added_by_user')->references('id')->on('users');
+            $table->integer('accepted_by_user')->unsigned();
+            $table->foreign('accepted_by_user')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
---
+++ database/migrations/2019_12_07_062533_create_quiz_questions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuizQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quiz_questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('quiz_id')->unsigned();
+            $table->foreign('quiz_id')->references('id')->on('quizzes');
+            $table->integer('question_id')->unsigned();
+            $table->foreign('question_id')->references('id')->on('questions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quiz_questions');
+    }
+}
